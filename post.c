a035ae2a2e9085fb06a04483ea237228cb944c1b
songmii <101651056+songmii@users.noreply.github.com>
2023-05-23 16:19:54 +0900

    Update post.c

--- post.c
+++ post.c
@@ -1,69 +1,70 @@
-// #define _CRT_SECURE_NO_WARNINGS
+// #include <stdio.h>
+#include "writePost.c"
+#include "showPost.c"
+#include "myPost.c"
+#include <sys/wait.h>
+#include <sys/types.h>
+#include "sugang_doumi.h"
 
-#include <stdio.h>
-#include <string.h>
-#include <time.h>
-#include <stdlib.h>
-
-typedef struct {
-	int num;
-	char title[50];
-	char writer[30];
-	char mtime[40];
-} Post;
-
-// int num = 1;
-
-int write_post(Post* posts,int* num)
-// int main() 
-{
-	// 사용자가 작성을 선택
-	char new_title[50];
-	FILE *fp;
-	char new_text[150];
-	/*
-	Post* posts;
-	posts = (Post*)malloc(0);
-	*/
+int main() {
+	int input_num;
 	
-	printf("title : ");
-	fgets(new_title, sizeof(new_title), stdin);
-	
-	
-	char filename[10];    // file open
-	sprintf(filename, "%d.txt", *num);
-	
-	fp = fopen(filename, "w");    
-	if (fp == NULL) {
-		perror("File open fail");
-		exit(0);
-	}
-
-	printf("text (종료는 q): \n");	// file writing
 	while (1) {
-		fgets(new_text, sizeof(new_text), stdin);
-		if (!strcmp(new_text, "q\n")) break;
-		
-		size_t len = strlen(new_text);    // 입력 끝의 개행 제거
-        	if (len > 0 && new_text[len - 1] == '\n')
-            	new_text[len - 1] = '\0';
-            	
-		fprintf(fp, "%s\n", new_text);
+		printf("----------------------------------------\n");
+		printf("1. 게시글 보기\n");
+		printf("2. 게시글 작성\n");
+		printf("3. 내가 쓴 글 보기\n");
+		printf("4. 메뉴 화면으로 돌아가기\n");
+		printf("----------------------------------------\n");
+	
+		while (1) {
+			printf(": ");
+			if ((scanf("%d", &input_num) != 1) || (input_num > 4) || (input_num < 1)) {
+				getchar();
+				printf("잘못된 입력입니다. 1, 2, 3, 4 중 하나를 입력하세요.\n");
+				continue;
+			}
+			else {
+				getchar();
+				break;
+			}
+		}
+	
+		switch(input_num) {
+			case 1:
+				clear_terminal();
+				show_post();
+				clear_terminal();
+				continue;
+			case 2:
+				clear_terminal();
+				write_post();
+				sleep(1);
+				clear_terminal();
+				continue;
+			case 3:
+				clear_terminal();
+				my_post();
+				clear_terminal();
+				continue;
+			case 4:
+				clear_terminal();
+				return 0;
+			default:
+				clear_terminal();
+				break;
+		}
+	
 	}
-	fclose(fp);
+	return 0;	
+}
 	
-	time_t t = time(NULL);    // make new_post
-	Post new_post;
-	new_post.num = *num;
-	strcpy(new_post.title, new_title);
-	strcpy(new_post.writer, "writer(student id)");
-	strcpy(new_post.mtime, ctime(&t));
-	
-	realloc(posts, ((*num)+1) * sizeof(Post));
-	posts[*num++] = new_post;
-	
-	printf("\n작성이 완료되었습니다.\n");
-	
-		
-	return 0;
+int clear_terminal() {
+	int pid;
+
+	pid = fork();
+	if(pid == 0){
+		execlp("clear", "clear", NULL);
+	}
+	wait(NULL);
 }
